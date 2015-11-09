8a631b345cc11dbc959e7649ed96408d3ce2f351
Sky Liu <blackskygg@gmail.com>
2015-11-09 12:33:14 +0800

    fixed: syntax: can't recognize 0

--- syntax.h
+++ syntax.h
@@ -8,7 +8,7 @@
 #define R_DOT "\\."
 #define R_OPTION(_R, ...) "("  _R  __VA_ARGS__ ")?"
 
-#define R_DEC_NUM "^" "[1-9][0-9]*" R_OPTION(R_DOT,R_DEC_DIGITS)
+#define R_DEC_NUM "^" "[1-9][0-9]*" R_OPTION(R_DOT,R_DEC_DIGITS) "|0"
 #define R_HEX_NUM "^(0x)" R_HEX_DIGITS R_OPTION(R_DOT,R_HEX_DIGITS)
 #define R_OCT_NUM "^0" R_OCT_DIGITS R_OPTION(R_DOT,R_OCT_DIGITS)
 #define R_BIN_NUM "^" R_BIN_DIGITS R_OPTION(R_DOT,R_BIN_DIGITS) "[b]"
